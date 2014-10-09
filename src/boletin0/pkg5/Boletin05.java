9ea856cf78623f49e147572f951b1ddc27e248cc
aferreiradominguez <aferreiradominguez@eniac27.danielcastelao.org>
2014-10-09 12:27:11 +0200

    pasoporvalor arreglar classs

---
+++ src/boletin0/pkg5/Boletin05.java
@@ -0,0 +1,24 @@
+
+package boletin0.pkg5;
+
+/**
+ *
+ * @author aferreiradominguez
+ */
+public class Boletin05 {
+
+    public static void main(String[] args) {
+   int x=4;
+   //visualizar x
+   //chamo método mudarDatoValor(x)
+   //visualizar x again
+   System.out.println("x="+x); 
+   String paso = null;
+   
+   
+   
+   
+   
+    }
+    
+}
---
+++ src/boletin0/pkg5/pasoPorValor.java
@@ -0,0 +1,13 @@
+
+package boletin0.pkg5;
+
+/**
+ *
+ * @author aferreiradominguez
+ */
+public class pasoPorValor {
+    public void mudarDatoValor(int i){
+        i=23;
+     }
+    
+}
